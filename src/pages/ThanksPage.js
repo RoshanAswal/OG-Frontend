c5c0a49d6348f0242074dbd2db7ec9d32c973828
RoshanAswal <aswalroshan2002@gmail.com>
2023-05-09 12:42:53 +0530

    finale

--- src/pages/ThanksPage.js
+++ src/pages/ThanksPage.js
@@ -1,19 +1,8 @@
 import { Navbar } from "../components/Navbar"
+import { Link } from "react-router-dom";
 import certi from "../images/certificate.png";
-import insta from "../images/instagram.png";
-import yt from "../images/youtube.png";
-import discord from "../images/discord.png";
 
 export const ThanksPage=()=>{
-    const goToInsta=()=>{
-        window.location.href="https://instagram.com/onlygeeks.club?igshid=ZGUzMzM3NWJiOQ==";
-    }
-    const goToYT=()=>{
-        window.location.href="";
-    }
-    const goToDiscord=()=>{
-        window.location.href="https://discord.gg/Px3E6KAr";
-    }
 
     return(
         <div className="thanksPage">
@@ -21,9 +10,7 @@ export const ThanksPage=()=>{
             <div id="thanks">
                 <img id="certi" src={certi} alt="hands"/>
                 <h1 id="thank">Thank you, Your submission is accepted</h1>
-                <img src={insta} alt="insta" onClick={goToInsta}/>
-                <img src={yt} alt="youtube" onClick={goToYT}/>
-                <img src={discord} alt="discord" onClick={goToDiscord}/>
+                <Link to="/">Go Back</Link>
             </div>
         </div>
     )
