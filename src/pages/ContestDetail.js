d060689aca8b706d0aa7bb9e335a5386f5081b02
RoshanAswal <aswalroshan2002@gmail.com>
2023-05-23 21:52:23 +0530

    postpone

--- src/pages/ContestDetail.js
+++ src/pages/ContestDetail.js
@@ -146,11 +146,11 @@ export const ContestDetail=(props)=>{
         if(parseInt(min)>limit){
             return false;
         }
-
-        if(loading===false && contestDetail.type==="upcoming"
-            && day==="Sat" && hour==="07" && period==="pm"){ // Contest starting time
-            return true;
-        }else return false;
+        return false;
+        // if(loading===false && contestDetail.type==="upcoming"
+        //     && day==="Sat" && hour==="07" && period==="pm"){ // Contest starting time
+        //     return true;
+        // }else return false;
     }
 
     const showButton=()=>{
